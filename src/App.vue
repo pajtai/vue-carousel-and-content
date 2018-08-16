89973b237ab69af125f2f64cf1208ad44e64ff4a
Peter Ajtai <peter.ajtai@soliddigital.com>
2018-08-16 14:50:52 -0700

    Badly working hovers

--- src/App.vue
+++ src/App.vue
@@ -5,45 +5,51 @@
 </template>
 
 <script>
-    import Carousel from "./components/Carousel.vue";
+import Carousel from "./components/Carousel.vue";
 
-    export default {
-        name: "app",
-        data: function() {
-            return {
-                slides: [
-                    {
-                        img: 'https://images.pexels.com/photos/874730/pexels-photo-874730.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                        title: 'First!',
-                        content: '<h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2>'
-                    },
-                    {
-                        img: 'https://images.pexels.com/photos/418626/pexels-photo-418626.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-                    },
-                    {
-                        img: 'https://images.pexels.com/photos/1323443/pexels-photo-1323443.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                        content: '<h2>three!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2>'
-                    }
-                ]
-            };
+export default {
+  name: "app",
+  data: function() {
+    return {
+      slides: [
+        {
+          img:
+            "https://images.pexels.com/photos/874730/pexels-photo-874730.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          title: "First!",
+          content:
+            "<h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2>"
         },
-        components: {
-            'vue-carousel': Carousel
+        {
+          img:
+            "https://images.pexels.com/photos/418626/pexels-photo-418626.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        },
+        {
+          img:
+            "https://images.pexels.com/photos/1323443/pexels-photo-1323443.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          content:
+            "<h2>three!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2><h2>one!</h2>"
         }
+      ]
     };
+  },
+  components: {
+    "vue-carousel": Carousel
+  }
+};
 </script>
 
 <style lang="scss">
-  html, body {
-    border: 0;
-    padding: 0;
-    margin: 0;
-  }
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+html,
+body {
+  border: 0;
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -18,278 +18,328 @@
 </template>
 
 <script>
-    export default {
-        name: "HelloWorld",
-        props: ['slides'],
-        data: function() {
-            return {
-                current: 0,
-                firstSlideOffset: 100,
-                animationClass: '',
-                listening: false
-            }
-        },
-        methods: {
-            doneWithAnimation() {
-                if (!this.listening) {
-                    return;
-                }
-
-                switch (this.listening) {
-                    case 'next':
-                        const first = this.slides.shift();
-                        this.slides.push(first);
-                        console.log('done next');
-                        break;
-                    case 'prev':
-                        const last = this.slides.pop();
-                        this.slides.unshift(last);
-                        console.log('done prev');
-                        break;
-                }
-                this.animationClass = '';
-                this.listening = false;
-            },
-
-            next() {
-                scrollTo(this.$refs.box,0, 250, () => {
-                    this.listening = 'next';
-                    this.animationClass = 'slide-out';
-                });
-
-                console.log('next');
-            },
-            prev() {
-                scrollTo(this.$refs.box, 0, 250, () => {
-                    this.listening = 'prev';
-                    this.animationClass = 'slide-in';
-                });
-
-                console.log('prev');
-            },
-
-            peaknext() {
-                console.log('pn');
-                this.animationClass = 'peak-out';
-            },
-            peakprev() {
-                console.log('pp');
-                this.animationClass = 'peak-in';
-            },
-            unpeakprev() {
-                this.animationClass = 'un-peak-prev';
-            },
-            unpeaknext() {
-                this.animationClass = 'un-peak-next';
-            },
-
-            down() {
-                const box = this.$refs.box;
-                scrollTo(box, box.scrollTop + 100, 250);
-            }
-        }
+export default {
+  name: "HelloWorld",
+  props: ["slides"],
+  data: function() {
+    return {
+      current: 0,
+      firstSlideOffset: 100,
+      animationClass: "",
+      animationClassCached: "",
+      listening: false
     };
+  },
+  methods: {
+    doneWithAnimation() {
+      if (!this.listening) {
+        return;
+      }
 
-    function scrollTo(element, to = 0, duration= 1000, callback) {
+      let first, last;
+      switch (this.listening) {
+        case "next":
+          first = this.slides.shift();
+          this.slides.push(first);
+          console.log("done next");
+          break;
+        case "prev":
+          last = this.slides.pop();
+          this.slides.unshift(last);
+          console.log("done prev");
+          break;
+      }
+      this.animationClass = this.animationClassCached;
+      this.listening = false;
+    },
 
-        const start = element.scrollTop;
-        const change = to - start;
-        const increment = 20;
-        let currentTime = 0;
+    next() {
+      scrollTo(this.$refs.box, 0, 250, () => {
+        this.listening = "next";
+        this.animationClassCached = this.animationClass;
+        this.animationClass = "slide-out";
+      });
 
-        const animateScroll = (() => {
+      console.log("next");
+    },
+    prev() {
+      scrollTo(this.$refs.box, 0, 250, () => {
+        this.listening = "prev";
+        this.animationClassCached = this.animationClass;
+        this.animationClass = "slide-in";
+      });
 
-            currentTime += increment;
+      console.log("prev");
+    },
 
-            element.scrollTop = Math.easeInOutQuad(currentTime, start, change, duration);
+    peaknext() {
+      console.log("pn");
+      this.animationClass = "peak-out";
+    },
+    peakprev() {
+      console.log("pp");
+      this.animationClass = "peak-in";
+    },
+    unpeakprev() {
+      this.animationClass = "un-peak-prev";
+      this.animationClassCached = '';
+    },
+    unpeaknext() {
+      this.animationClass = "un-peak-next";
+      this.animationClassCached = '';
+    },
 
-            if (currentTime < duration  && change !== 0) {
-                setTimeout(animateScroll, increment);
-            } else {
-                if (callback) {
-                    element.scrollTop = to;
-                    callback();
-                }
-            }
-        });
-
-        animateScroll();
+    down() {
+      const box = this.$refs.box;
+      scrollTo(box, box.scrollTop + 100, 250);
     }
+  }
+};
 
-    //t = current time
-    //b = start value
-    //c = change in value
-    //d = duration
-    Math.easeInOutQuad = function (t, b, c, d) {
+function scrollTo(element, to = 0, duration = 1000, callback) {
+  const start = element.scrollTop;
+  const change = to - start;
+  const increment = 20;
+  let currentTime = 0;
 
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-    };
+  const animateScroll = () => {
+    currentTime += increment;
 
+    element.scrollTop = Math.easeInOutQuad(
+      currentTime,
+      start,
+      change,
+      duration
+    );
 
+    if (currentTime < duration && change !== 0) {
+      setTimeout(animateScroll, increment);
+    } else {
+      if (callback) {
+        element.scrollTop = to;
+        callback();
+      }
+    }
+  };
+
+  animateScroll();
+}
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad = function(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t + b;
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    .nav {
-        position: fixed;
-        cursor: pointer;
-        top: 50%;
-        font-size: 36px;
-    }
-    .down {
-        position: absolute;
-        bottom: 0%;
-        left: 50%;
-        font-size: 48px;
-        cursor: pointer;
-    }
-    .prev {
-        left: 0;
-    }
-    .next {
-        right: 0;
-    }
-    .box {
-        overflow-x: hidden;
-        position: relative;
-        width: 100%;
-        height: 100vh;
-    }
-    .slider {
-        display: flex;
-    }
-    .slide {
-        /* make sure the width is honored */
-        flex-shrink: 0;
-        width: 100%;
-    }
+.nav {
+  position: fixed;
+  cursor: pointer;
+  top: 50%;
+  font-size: 36px;
+}
+.down {
+  position: absolute;
+  bottom: 0%;
+  left: 50%;
+  font-size: 48px;
+  cursor: pointer;
+}
+.prev {
+  left: 0;
+}
+.next {
+  right: 0;
+}
+.box {
+  overflow-x: hidden;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+.slider {
+  display: flex;
+}
+.slide {
+  /* make sure the width is honored */
+  flex-shrink: 0;
+  width: 100%;
+}
 
-    .image, .content {
-        transform: translateX(-100%);
-    }
+.image,
+.content {
+  transform: translateX(-100%);
+}
 
-    .image {
-        position:absolute;
-        width:100%;
-        height:100%;
-        background-size:cover;
-        background-position: center;
-        display: flex;
-        align-items: flex-end;
-        justify-content: center;
-    }
-    .title {
-        font-size: 120px;
-        margin-bottom: 10%;
-    }
-    .content {
-        position:absolute;
-        width:100%;
-        top:100%;
-        border-left: 1px solid red;
-        border-right: 1px solid red;
-    }
+.image {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+}
+.title {
+  font-size: 120px;
+  margin-bottom: 10%;
+}
+.content {
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  border-left: 1px solid red;
+  border-right: 1px solid red;
+}
 
-    .slide-in {
-        animation: slide-in 0.5s forwards;
-        -webkit-animation: slide-in 0.5s forwards;
-    }
+.slide-in {
+  animation: slide-in 0.5s forwards;
+  -webkit-animation: slide-in 0.5s forwards;
+}
 
-    .slide-out {
-        animation: slide-out 0.5s forwards;
-        -webkit-animation: slide-out 0.5s forwards;
-    }
+.slide-out {
+  animation: slide-out 0.5s forwards;
+  -webkit-animation: slide-out 0.5s forwards;
+}
 
-    @keyframes slide-in {
-        0% { transform: translateX(-90%); }
-        100% { transform: translateX(0%); }
-    }
+@keyframes slide-in {
+  0% {
+    transform: translateX(-90%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
+}
 
-    @-webkit-keyframes slide-in {
-        0% { -webkit-transform: translateX(-90%); }
-        100% { -webkit-transform: translateX(0%); }
-    }
+@-webkit-keyframes slide-in {
+  0% {
+    -webkit-transform: translateX(-90%);
+  }
+  100% {
+    -webkit-transform: translateX(0%);
+  }
+}
 
-    @keyframes slide-out {
-        0% { transform: translateX(-110%); }
-        100% { transform: translateX(-200%); }
-    }
+@keyframes slide-out {
+  0% {
+    transform: translateX(-110%);
+  }
+  100% {
+    transform: translateX(-200%);
+  }
+}
 
-    @-webkit-keyframes slide-out {
-        0% { -webkit-transform: translateX(-110%); }
-        100% { -webkit-transform: translateX(-200%); }
-    }
+@-webkit-keyframes slide-out {
+  0% {
+    -webkit-transform: translateX(-110%);
+  }
+  100% {
+    -webkit-transform: translateX(-200%);
+  }
+}
 
+.peak-in {
+  animation: peak-in 0.5s forwards;
+  -webkit-animation: peak-in 0.5s forwards;
+}
 
-    .peak-in {
-        animation: peak-in 0.5s forwards;
-        -webkit-animation: peak-in 0.5s forwards;
-    }
+.peak-out {
+  animation: peak-out 0.5s forwards;
+  -webkit-animation: peak-out 0.5s forwards;
+}
 
-    .peak-out {
-        animation: peak-out 0.5s forwards;
-        -webkit-animation: peak-out 0.5s forwards;
-    }
+.un-peak {
+  animation: un-peak 0.5s forwards;
+  -webkit-animation: un-peak 0.5s forwards;
+}
 
-    .un-peak {
-        animation: un-peak 0.5s forwards;
-        -webkit-animation: un-peak 0.5s forwards;
-    }
+@keyframes peak-in {
+  100% {
+    transform: translateX(-90%);
+  }
+}
 
-    @keyframes peak-in {
-        100% { transform: translateX(-90%); }
-    }
+@-webkit-keyframes peak-in {
+  100% {
+    -webkit-transform: translateX(-90%);
+  }
+}
 
-    @-webkit-keyframes peak-in {
-        100% { -webkit-transform: translateX(-90%); }
-    }
+@keyframes peak-out {
+  100% {
+    transform: translateX(-110%);
+  }
+}
 
-    @keyframes peak-out {
-        100% { transform: translateX(-110%); }
-    }
+@-webkit-keyframes peak-out {
+  100% {
+    -webkit-transform: translateX(-110%);
+  }
+}
 
-    @-webkit-keyframes peak-out {
-        100% { -webkit-transform: translateX(-110%); }
-    }
+@keyframes un-peak {
+  100% {
+    transform: translateX(-100%);
+  }
+}
 
-    @keyframes un-peak {
-        100% { transform: translateX(-100%); }
-    }
+@-webkit-keyframes un-peak {
+  100% {
+    -webkit-transform: translateX(-100%);
+  }
+}
 
-    @-webkit-keyframes un-peak {
-        100% { -webkit-transform: translateX(-100%); }
-    }
+.un-peak-prev {
+  animation: un-peak-prev 0.5s forwards;
+  -webkit-animation: un-peak-prev 0.5s forwards;
+}
 
-    .un-peak-prev {
-        animation: un-peak-prev 0.5s forwards;
-        -webkit-animation: un-peak-prev 0.5s forwards;
-    }
+@keyframes un-peak-prev {
+  0% {
+    transform: translateX(-90%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
 
-    @keyframes un-peak-prev {
-        0% { transform: translateX(-90%); }
-        100% { transform: translateX(-100%); }
-    }
+@-webkit-keyframes un-peak-prev {
+  0% {
+    -webkit-transform: translateX(-90%);
+  }
+  100% {
+    -webkit-transform: translateX(-100%);
+  }
+}
 
-    @-webkit-keyframes un-peak-prev {
-        0% { -webkit-transform: translateX(-90%); }
-        100% { -webkit-transform: translateX(-100%); }
-    }
+.un-peak-next {
+  animation: un-peak-next 0.5s forwards;
+  -webkit-animation: un-peak-next 0.5s forwards;
+}
 
-    .un-peak-next {
-        animation: un-peak-next 0.5s forwards;
-        -webkit-animation: un-peak-next 0.5s forwards;
-    }
+@keyframes un-peak-next {
+  0% {
+    transform: translateX(-110%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
 
-    @keyframes un-peak-next {
-        0% { transform: translateX(-110%); }
-        100% { transform: translateX(-100%); }
-    }
-
-    @-webkit-keyframes un-peak-next {
-        0% { -webkit-transform: translateX(-110%); }
-        100% { -webkit-transform: translateX(-100%); }
-    }
+@-webkit-keyframes un-peak-next {
+  0% {
+    -webkit-transform: translateX(-110%);
+  }
+  100% {
+    -webkit-transform: translateX(-100%);
+  }
+}
 </style>
